bd6a4e5281bc8dfb863e9e3df4166f27fe40bfe4
Abhay Upadhaya <eppetsitter@gmail.com>
2023-06-12 19:02:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+from tkinter import ttk
 import tkinter as tk       
 
 class Application(tk.Frame):              
     def __init__(frame, master=None):
         tk.Frame.__init__(frame, master)   
         frame.grid(ipadx=300, ipady=200)                       
-        frame.createWidgets()
-        frame.grid_rowconfigure(0, weight=1)
-        frame.grid_columnconfigure(0, weight=1)
-
-    def createWidgets(quit):
-        quit.quitButton = tk.Button(quit, text='Quit',
-            command=quit.quit)            
-        quit.quitButton.grid(row=0, column=0)            
+        label = ttk.Label(text='Select a Project')
+        programs = ttk.Combobox(state="readonly", values=['Project1', 'Project2', 'Project3', 'Project4'], textvariable = 'Select a Project')
+        label.grid()
+        programs.grid()
+        
+        
 
 app = Application()                       
 app.master.title('TimeSheet++')    
