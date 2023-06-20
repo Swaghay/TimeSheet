559f90fcceacfdf96ae365c62fbca24f929f441f
Abhay Upadhaya <eppetsitter@gmail.com>
2023-06-20 17:35:10 -0500

    saves stuff now

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from tkinter import ttk
-import tkinter as tk       
+import tkinter as tk      
+from tkinter import messagebox 
+import pickle
+import os
 
-programList = ['Create New Project', 'Project1', 'Project2', 'Project3', 'Project4']
+def open_projects_file():
+    try:
+        with open(r"C:\Users\Abhay\Desktop\TimeSheet++_Data\data.pickle", 'rb') as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error Loading Object", ex)
+
+try:
+    projectList = open_projects_file()
+except:
+    projectList = ['Create New Project', 'Project1', 'Project2', 'Project3', 'Project4']
 
 class Application(tk.Frame):              
     def __init__(mainFrame, master=None):
@@ -9,14 +22,15 @@ class Application(tk.Frame):
         mainFrame.grid(padx=70,pady=70)
 
 def mainFrameButtonFunction():
-    if programs.get() == 'Create New Project':
+    if projects.get() == 'Create New Project':
         def newProjectWindowButtonFunction():
-            if newProjectTextBox.get() != '' and newProjectTextBox.get() not in programs['values']:
-                programs['values'] += (newProjectTextBox.get(),)
+            if newProjectTextBox.get() != '' and newProjectTextBox.get() not in projects['values']:
+                projects['values'] += (newProjectTextBox.get(),)
+                projects.set('')
                 newProjectWindow.destroy()
 
         newProjectWindow = tk.Toplevel(width=300, height = 275)
-        newProjectWindow.title(programs.get())
+        newProjectWindow.title(projects.get())
         newProjectWindow.resizable(False, False)
 
         newProjectTextBox = ttk.Entry(newProjectWindow)
@@ -28,38 +42,58 @@ def mainFrameButtonFunction():
 
         newProjectWindow.mainloop()
         
-    elif programs.get() != "":
-        currProgram = programs.get()
+    elif projects.get() != "":
+        currProject = projects.get()
 
-        def deleteProgramButtonFunction():
-            newProgramList = []
-            for program in programs['values']:
-                if program != currProgram:
-                    newProgramList.append(program)
-            programs['values'] = newProgramList
-            programs.delete(0,'end')
-            programs.set('')
-            programWindow.destroy()
+        def deleteProjectButtonFunction():
+            newProjectList = []
+            for project in projects['values']:
+                if project != currProject:
+                    newProjectList.append(project)
+            projects['values'] = newProjectList
+            projects.delete(0,'end')
+            projects.set('')
+            projectWindow.destroy()
 
-        programWindow = tk.Toplevel(width=300, height=300)
-        programWindow.title(programs.get())
-        programWindow.resizable(False, False)
+        projectWindow = tk.Toplevel(width=300, height=300)
+        projectWindow.title(projects.get())
+        projectWindow.resizable(False, False)
 
-        deleteProgramButton = ttk.Button(programWindow, command=deleteProgramButtonFunction, text='Delete')
-        deleteProgramButton.place(relx=.5, rely = .5, anchor='center')
+        deleteProjectButton = ttk.Button(projectWindow, command=deleteProjectButtonFunction, text='Delete')
+        deleteProjectButton.place(relx=.5, rely = .5, anchor='center')
 
-        programWindow.mainloop()
+        projectWindow.mainloop()
 
 
 app = Application()
 
 label = ttk.Label(app, text='Select a Project')
-programs = ttk.Combobox(app, state="readonly", values= programList, textvariable = 'Select a Project')
+projects = ttk.Combobox(app, state="readonly", values= projectList, textvariable = 'Select a Project')
 button = ttk.Button(app, command = mainFrameButtonFunction, text='Open')
 label.grid(row=0, column=0, pady=10)
-programs.grid(row=1, column=0)
+projects.grid(row=1, column=0)
 button.grid(row=2, column=0, pady=10)     
 
+def closeApp():
+    answer = messagebox.askyesnocancel("Quit", "Do you want to save and quit?")
+    if answer:
+        save_projects(projects['values'])
+        app.master.destroy()
+    elif answer == False:
+        app.master.destroy()
+
+def save_projects(obj):
+    try:
+        os.mkdir(r'C:\Users\Abhay\Desktop\TimeSheet++_Data')
+    except OSError as er:
+            print(er)
+    try:
+        with open(r"C:\Users\Abhay\Desktop\TimeSheet++_Data\data.pickle", "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error Saving Object!", ex)
+    
 app.master.title('TimeSheet++')    
 app.master.resizable(False, False)
+app.master.protocol("WM_DELETE_WINDOW", closeApp)
 app.mainloop()  
